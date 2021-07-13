6b34ef15ca7474297ccec08b7f167a790d43d6cd
Benjamin Martin <DNAblue2112@gmail.com>
2021-07-13 20:26:29 +0930

    Code cleanup

--- src/main/java/me/dnablue2112/Main.java
+++ src/main/java/me/dnablue2112/Main.java
@@ -1,13 +1,10 @@
 package me.dnablue2112;
 
-import com.opencsv.exceptions.CsvException;
-
 import java.io.File;
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CsvException {
+    public static void main(String[] args) {
         new VotesLoader(new File(args[0]));
     }
 
